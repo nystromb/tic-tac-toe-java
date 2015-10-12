0d52fa3f5b4e933890a50c07b0eb3f7bba8dd2cc
nystromb <bnystrom@siu.edu>
2015-10-12 11:46:04 -0500

    Determines win on the board

--- TicTacToeGame/src/Board.java
+++ TicTacToeGame/src/Board.java
@@ -3,13 +3,14 @@ import java.util.List;
 
 public class Board {
 	private GameToken [] spots;
+	private int [][] winIndexes = { {1,2,3}, {4,5,6}, {7,8,9}, {1,4,7}, {2,5,8}, {3,6,9}, {1,5,9}, {3,5,7} };
 	
 	public Board (int size){
 		spots = new GameToken[size];
-		clearAllSpots();
+		clearAll();
 	}
 	
-	private void clearAllSpots() {
+	private void clearAll() {
 		for(int index = 1; index <= spots.length; index++)
 			spots[index-1] = GameToken.EMPTY;
 	}
@@ -30,5 +31,20 @@ public class Board {
 				emptySpots.add(index+1);
 		
 		return emptySpots;
+	}
+
+	public boolean win(GameToken piece) {
+		for(int[] indexes: winIndexes)
+			if(isGameWinningLineup(indexes, piece))
+				return true;
+				
+		return false;
+	}
+	
+	public boolean isGameWinningLineup(int[] indexes, GameToken piece){
+		for(int index : indexes)
+			if(spots[index-1] != piece)
+				return false;
+		return true;
 	}
 }
