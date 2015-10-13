bb9c630b1520e80d9c4492cbbb6e18be6932e67e
nystromb <bnystrom@siu.edu>
2015-10-13 12:00:23 -0500

    Minimax implemented

--- TicTacToeGame/src/UnbeatablePlayer.java
+++ TicTacToeGame/src/UnbeatablePlayer.java
@@ -1,16 +1,53 @@
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
 
-public class UnbeatablePlayer extends Player implements MiniMax {
-
-	public int getMove(Board board) {
-		return minimax(board, 0);
+public class UnbeatablePlayer extends Player {
+	private int bestMove;
+	private int bestScore;
+	
+	public int getMove(GameModel game) {
+		return minimax(game, 0);
 	}
 	
-	public int minimax(Board board, int depth) {
+	private int minimax(GameModel game, int depth) {
+		if(game.isOver())
+			score(game.board, depth);
 		
-		return 0;
+		List<Integer> moves = game.getEmptySpots();
+		Map<Integer, Integer> scores = new Hashtable<Integer, Integer>(moves.size());
+		
+		for(Integer move : moves){
+			GameModel copy = game.newGameState(move.intValue());
+			scores.put(move, minimax(copy, depth+1));
+		}
+		
+		if(game.currentPlayer.getPiece() == GameToken.X){
+			bestScore = -100;
+			for(Integer move : moves){
+				if(scores.get(move) > bestScore){
+					bestScore = scores.get(move);
+					bestMove = move;
+				}
+			}
+		}else { 
+			bestScore = 100;
+			for(Integer move : moves){
+				if(scores.get(move) < bestScore){
+					bestScore = scores.get(move);
+					bestMove = move;
+				}
+			}
+		}
+		
+		if(depth == 0){
+			return bestMove;
+		}else{
+			return bestScore;
+		}
 	}
 	
-	public int score(Board board, int depth) {
+	private int score(Board board, int depth) {
 		if (board.win(GameToken.X)) {
 			return 10 - depth;
 		} else if (board.win(GameToken.O)) {
