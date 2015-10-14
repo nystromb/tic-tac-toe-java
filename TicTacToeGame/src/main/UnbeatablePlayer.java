529ed37eb53821fb260119f4bbadd605c4fe40af
nystromb <bnystrom@siu.edu>
2015-10-14 18:10:57 -0500

    uses a LinkedHashMap to store scores

--- TicTacToeGame/src/main/UnbeatablePlayer.java
+++ TicTacToeGame/src/main/UnbeatablePlayer.java
@@ -1,61 +1,54 @@
 package main;
 
-import java.util.Hashtable;
-import java.util.List;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class UnbeatablePlayer extends Player {
-	private int bestMove;
-	private int bestScore;
+	Entry<Integer, Integer> bestEntry;
+	int bestMove = 0;
+	int bestScore = 0;
 	
 	public int getMove(GameModel game) {
-		return minimax(game, 0);
+		return minimax(new GameModel(game), 0);
 	}
 	
 	private int minimax(GameModel game, int depth) {
 		if(game.isOver())
-			score(game.board, depth);
+			return score(game.board, depth);
+	
+		Map<Integer, Integer> scores = new LinkedHashMap<Integer, Integer>();
 		
-		List<Integer> moves = game.getEmptySpots();
-		Map<Integer, Integer> scores = new Hashtable<Integer, Integer>(moves.size());
-		
-		for(Integer move : moves){
-			GameModel copy = game.newGameState(move.intValue());
+		for(Integer move : game.getEmptySpots()){
+			GameModel copy = game.newGameState(move);
 			scores.put(move, minimax(copy, depth+1));
 		}
-		
-		if(game.currentPlayer.getPiece() == GameToken.X){
-			bestScore = -100;
-			for(Integer move : moves){
-				if(scores.get(move) > bestScore){
-					bestScore = scores.get(move);
-					bestMove = move;
-				}
-			}
-		}else { 
-			bestScore = 100;
-			for(Integer move : moves){
-				if(scores.get(move) < bestScore){
-					bestScore = scores.get(move);
-					bestMove = move;
-				}
-			}
+
+		if(game.currentPlayer.getPiece() == GameToken.X) {
+			for(Entry<Integer, Integer> entry : scores.entrySet())
+				if(bestEntry == null || entry.getValue() > bestEntry.getValue())
+					bestEntry = entry;
+		}
+		else {
+			for(Entry<Integer, Integer> entry : scores.entrySet())
+				if(bestEntry == null || entry.getValue() < bestEntry.getValue())
+					bestEntry = entry;
 		}
 		
-		if(depth == 0){
-			return bestMove;
-		}else{
-			return bestScore;
-		}
+		if(depth == 0)
+			//System.out.println(scores);
+			return bestEntry.getKey();
+		else
+			return bestEntry.getValue();
+		
 	}
 	
 	private int score(Board board, int depth) {
-		if (board.win(GameToken.X)) {
+		if (board.win(GameToken.X))
 			return 10 - depth;
-		} else if (board.win(GameToken.O)) {
+		else if (board.win(GameToken.O))
 			return depth - 10;
-		} else {
+		else
 			return 0;
-		}
 	}
 }
