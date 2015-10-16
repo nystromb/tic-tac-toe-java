420ff68d8fbb1c2fbef0e73d2cc11f3ec620025b
nystromb <bnystrom@siu.edu>
2015-10-16 14:18:26 -0500

    passes winningIndexes through super()

--- TicTacToeGame/src/main/ThreeByThreeBoard.java
+++ TicTacToeGame/src/main/ThreeByThreeBoard.java
@@ -3,7 +3,6 @@ package main;
 public class ThreeByThreeBoard extends Board {
 	
 	public ThreeByThreeBoard () {
-		super(9);
-		this.winIndexes = new int[][] { {1,2,3}, {4,5,6}, {7,8,9}, {1,4,7}, {2,5,8}, {3,6,9}, {1,5,9}, {3,5,7} };
+		super(9, new int[][] { {1,2,3}, {4,5,6}, {7,8,9}, {1,4,7}, {2,5,8}, {3,6,9}, {1,5,9}, {3,5,7} });
 	}
 }
