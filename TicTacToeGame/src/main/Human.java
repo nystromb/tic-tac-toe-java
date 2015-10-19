82beb22642c32b6c8874cf5b5696c6e3c07b34b3
nystromb <bnystrom@siu.edu>
2015-10-19 15:24:49 -0500

    Prompts user for move

--- TicTacToeGame/src/main/Human.java
+++ TicTacToeGame/src/main/Human.java
@@ -10,6 +10,7 @@ public class Human extends Player {
 	
 	@Override
 	public int getMove(GameModel game) {
+		cli.promptHumanForMove();
 		while(true){
 			move = cli.getGameInput(); 
 			if(game.moveIsValid(move)) return move;
