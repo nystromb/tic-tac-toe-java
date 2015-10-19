8ea0dbbab99fda00b961b1954b516b65a8d664cc
nystromb <bnystrom@siu.edu>
2015-10-19 11:02:40 -0500

    Human player knows about the CLI

--- TicTacToeGame/src/main/Human.java
+++ TicTacToeGame/src/main/Human.java
@@ -4,29 +4,14 @@ import java.util.Scanner;
 
 public class Human extends Player {
 	private int move;
-	Scanner input;
+	CommandLineInterface cli;
 	
-	public Human(Scanner input){
-		this.input = input;
+	public Human(CommandLineInterface cli){
+		this.cli = cli;
 	}
 	
 	@Override
 	public int getMove(GameModel game) {
-		System.out.println("What is your next move?");
-		
-		while (true) {
-	        System.out.print("> ");
-	        String userInput = input.next();
-	        try {
-	        	move = Integer.parseInt(userInput);
-	        } catch(NumberFormatException e){
-	        	System.out.println("invalid");
-	        }
-	        
-	        if(game.moveIsValid(move))
-	        	break;
-	    }
-		
 		return move;
 	}
 
