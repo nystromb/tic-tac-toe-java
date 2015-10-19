e518e25ed6d3522f297de8267cfacd8b75a58c6e
nystromb <bnystrom@siu.edu>
2015-10-19 11:00:51 -0500

    Rewrote printBoard with tests, deleted untested functions

--- TicTacToeGame/src/main/CommandLineInterface.java
+++ TicTacToeGame/src/main/CommandLineInterface.java
@@ -1,15 +1,17 @@
 package main;
-import java.util.InputMismatchException;
+
+import java.io.PrintStream;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.Scanner;
 
 public class CommandLineInterface implements Observer {
 	Scanner input;
-	int userMode;
+	PrintStream output;
 	
-	public CommandLineInterface(Scanner input){
+	public CommandLineInterface(Scanner input, PrintStream output){
 		this.input = input;
+		this.output = output;
 	}
 	
 	public void update(Observable o, Object board) {
@@ -18,34 +20,15 @@ public class CommandLineInterface implements Observer {
 
 	private void printBoard(Board board) {
 		for(int index = 1; index <= board.getCellCount(); index++){
-			if(board.getMove(index) == GameToken.EMPTY){
-				System.out.print(index + " ");
-			}else{
-				System.out.print(board.getMove(index) + " ");
-			}
+			if(board.getMove(index) == GameToken.EMPTY)
+				output.print(index);
+			else
+				output.print(board.getMove(index));
 			
-			if(index % Math.sqrt(board.getCellCount()) == 0){
-				System.out.println("");
-			}
+			if((index % Math.sqrt(board.getCellCount())) == 0)
+				output.println("");
+			else
+				output.print("|");
 		}
-		System.out.println("");
-	}
-
-	public int getModeFromUser() {
-		System.out.print("What game mode would you like to play?\n");
-		System.out.print(" 1. Human vs. Human\n 2. Human vs. Computer\n 3. Computer vs. Computer\n\n");
-	    while (true) {
-	        System.out.print("> ");
-	        String userInput = input.next();
-	        try {
-	        	userMode = Integer.parseInt(userInput);
-	        } catch(NumberFormatException e){
-	        	
-	        }
-	        
-	        if(userMode == 1 || userMode == 2 || userMode == 3)
-	        	break;
-	    }
-		return userMode;
 	}
 }
